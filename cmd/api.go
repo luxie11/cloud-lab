fbbe56cae3fa510ab1b5d56401f2a152f1428329
luxie11 <lukmat@NBKL4172098.clt.lt.centric.lan>
2019-11-28 10:33:21 +0200

    made api service

--- cmd/api.go
+++ cmd/api.go
@@ -21,7 +21,7 @@ var httpServerCmd = &cobra.Command{
 func server() {
 	logger := log.New(os.Stdout, "[api] ", log.Ltime)
 	r := mux.NewRouter()
-	r.HandleFunc("/username", controller.GithubUsernameHandler).Methods("GET")
+	r.HandleFunc("/luxie11", controller.GithubUsernameHandler).Methods("GET")
 
 	// TODO. Add a handler here which will use controller.Sha256Handler function for HTTP "GET" method. Use curly brackets for path matching, like this: /{username}
 
