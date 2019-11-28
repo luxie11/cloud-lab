10c6431b12a40d6b2b23dc7f4b2500a860211ce3
luxie11 <lukmat@NBKL4172098.clt.lt.centric.lan>
2019-11-28 11:04:30 +0200

    made api service 8

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -4,20 +4,24 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"github.com/luxie11/cloud-lab/internal/hashes"
+	"github.com/gorilla/mux"
 )
 
+//ttt
 func Sha256Handler(w http.ResponseWriter, r *http.Request) {
 	// to get the username, use the following:
-	// username := mux.Vars(r)["username"]
+	username := mux.Vars(r)["username"]
 
 	// to calculate sha256 hash of a string, use internal/hashes package and function GetHash i.e.: myhash, _ := hashes.GetHash("Sha256"
-
+	myhash, _ := hashes.GetHash("Sha256", username)
 	// to send the response, use the following:
-	// fmt.Fprint(w, hash)
+	fmt.Fprint(w, myhash)
 
 	w.WriteHeader(http.StatusOK)
 }
 
+//ttt
 func GithubUsernameHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, os.Getenv("GITHUB_USERNAME"))
